bdc4738120378255690e957c002db94878080350
AngelLaines <angelmoreno755@gmail.com>
2024-01-19 20:35:38 -0700

    first commit

---
+++ src/composables/useClima.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import {computed, ref} from 'vue'
+
+export default function useClima(){
+    const clima = ref({})
+    const cargando = ref(false)
+    const error = ref('')
+    const obtenerClima = async({ciudad,pais})=>{
+        cargando.value=true;
+        clima.value={}
+        error.value=''
+        const key = import.meta.env.VITE_API_KEY;
+
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+
+        try {
+            const {data} = await axios(url)
+            const {lat,lon} = data[0];
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+            const {data:resultado} = await axios(urlClima);
+            clima.value=resultado
+        } catch (err) {
+            error.value = 'Ciudad no encontrada.'
+        } finally{
+            cargando.value=false
+        }
+    }
+
+    const mostrarClima = computed(()=>{
+        return Object.values(clima.value).length>0;
+    })
+
+    const formatearTemperatura = temperatura =>parseInt(temperatura-273.15);
+    return {
+        obtenerClima,
+        clima,
+        mostrarClima,
+        cargando,
+        formatearTemperatura,
+        error,
+    }
+}
